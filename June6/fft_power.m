0559704ff6db1df4989c7c0e5cc8cef4f4c6a058
Nakshatra120 <99581284+Nakshatra120@users.noreply.github.com>
2023-07-06 19:06:30 -0700

    Initial commit

---
+++ June6/fft_power.m
@@ -0,0 +1,38 @@
+% import data
+filename = '20nm_a0.1_j0.08.txt';
+[A,delimiterOut]=importdata(filename);
+
+% pick out m.region1y () column
+my = A.data(:,6);
+
+% cut out first 20 percent of points
+L = length(my);
+L2 = round(L/5); 
+my = A.data(L2:L,6);
+
+% take FFT and calculate amplitude
+N = L-L2;    % number of points
+Fs = N/(100*10^(-9)); % sampling frequency for 100ns of data
+Y = fft(my);    % take fft
+P2 = abs(Y/N);  % two-sided spectrum
+P1 = P2(1:round(N/2));   % single-sided amplitude spectrum
+P1(2:end-1) = 2*P1(2:end-1);
+f3=[0:(length(Y)/2-1)]/10^(-7);     % frequencies for plot
+
+% calculate power
+p = abs(Y)/(length(Y)/2);
+p = p(1:length(Y)/2).^2;
+
+% plot amplitude
+figure(1);
+plot(f3,P1)
+title("Amplitude Spectrum")
+xlabel("f (Hz)")
+ylabel("Amplitude")
+
+% plot power
+figure(2);
+plot(f3,p) 
+title("Power Spectrum")
+xlabel("f (Hz)")
+ylabel("Power")
---
+++ Work jun 27/ReadMe.txt
@@ -0,0 +1,4 @@
+I am changing radii here ffrom 20nm to 200nm. Need to see for each radius at which current the power drops to approximately zero, for example, in order of 10^(-6). 
+
+Do it all on Nx,Ny = 256
+and on 2micron x 2micron
